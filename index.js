571501a3c2fbeb002d610b48f57516716f40a04f
xJuhix <49311688+xJuhix@users.noreply.github.com>
2021-02-11 22:05:58 +0100

    Made changes to project structure

--- index.js
+++ index.js
@@ -38,9 +38,9 @@ app.post('/sendmail', async (req, res) => {
 });
 
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(path.join(__dirname, './client/build')));
+	app.use(express.static(path.join(__dirname, 'client/build')));
 	app.get('*', function(req, res) {
-		res.sendFile(path.join(__dirname, './client/build', 'index.html'));
+		res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 	});
 }
 
