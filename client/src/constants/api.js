1e2bf3f750e8fbcb60f5d4af7fee0d33da6a15a1
xJuhix <49311688+xJuhix@users.noreply.github.com>
2021-02-12 00:18:47 +0100

    Fixing cors error

--- client/src/constants/api.js
+++ client/src/constants/api.js
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
-export const BASE_URL = 'http://wp-subdir.wittignotes.com/wp-json/wp/v2/posts';
+export const BASE_URL = 'https://wp-subdir.wittignotes.com/wp-json/wp/v2/posts';
 export const BOOK_URL = `${BASE_URL}?categories=3`;
 export const ECONOMICS_URL = `${BASE_URL}?categories=4`;
 export const PRODUCTIVY_URL = `${BASE_URL}?categories=5`;
