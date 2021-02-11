599f0f98147c0c0c394bc54c7bbbd1642c4f6565
xJuhix <49311688+xJuhix@users.noreply.github.com>
2021-02-11 22:01:27 +0100

    Testing directory access

--- server/index.js
+++ server/index.js
@@ -38,10 +38,11 @@ app.post('/sendmail', async (req, res) => {
 });
 
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(path.join(__dirname, '../client/build')));
+	app.use(express.static(path.join(__dirname, './client/build')));
 	app.get('*', function(req, res) {
-		res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+		res.sendFile(path.join(__dirname, './client/build', 'index.html'));
 	});
 }
+
 app.listen(PORT, () => console.log(`Listening on port ${PORT}!`));
 
