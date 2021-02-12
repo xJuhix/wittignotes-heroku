3867ae5f3e41a7dfd716ebb3f2a84e767c98b65f
xJuhix <49311688+xJuhix@users.noreply.github.com>
2021-02-12 03:00:08 +0100

    Attemting to fix contact error

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const corsOptions = {
 	}, credentials: true
 }
 app.use(cors(corsOptions));
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+//app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
 
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
