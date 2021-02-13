30a25e2c2e1c569cac379541b8c48ad5399540b3
xJuhix <49311688+xJuhix@users.noreply.github.com>
2021-02-13 22:06:51 +0100

    Fixing errors

--- client/src/constants/api.js
+++ client/src/constants/api.js
@@ -1,6 +1,6 @@
 /* eslint-disable prettier/prettier */
-export const BASE_URL = 'https://wp-subdir.wittignotes.com/wp-json/wp/v2/posts?';
-export const ALL_URL = 'https://wp-subdir.wittignotes.com/wp-json/wp/v2/posts?per_page=100';
+export const BASE_URL = 'https://wp-subdir.wittignotes.com/wp-json/wp/v2/posts';
+export const ALL_URL = `${BASE_URL}?per_page=100`;
 export const BOOK_URL = `${BASE_URL}?categories=3`;
 export const ECONOMICS_URL = `${BASE_URL}?categories=4`;
 export const PRODUCTIVY_URL = `${BASE_URL}?categories=5`;
